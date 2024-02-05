cc80d09983f031c4600ba53aff639772e9dc7449
Tom Hsieh <hsieh.tom@gmail.com>
2024-02-05 15:45:31 -0800

    js and html for to-do-list

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -5,12 +5,67 @@ get text from textbox
 add to array 
 console log the array
 */
-const todolist = [];
+
+// document.getElementById("enterKey").addEventListener("keydown", function(event) {
+//   // If the user presses the "Enter" key on the keyboard
+//   if (event.key === "Enter") {
+//     // Trigger the button element with a click
+//     document.getElementById("enterKey").click();
+//   }
+// });
+
+const todoList = [{
+  name: 'item 1', 
+  dueDate: '2023-01-05'
+}, {
+ name: 'item 2',
+ dueDate: '2023-01-05'
+}];
 function addTodo() {
   // this gets an element with the class js-name-input
   const inputElement = document.querySelector('.js-name-input');
   const name = inputElement.value;
-  todolist.push(name)
+
+  const dateInputElement = document.querySelector('.js-due-date-input');
+  const dueDate = dateInputElement.value;
+
+  todoList.push({
+    // name: name, 
+    // dueDate: dueDate
+    name,
+    dueDate
+  })
   inputElement.value = '';
-  console.log(todolist);
+  renderTodoList()
+}
+
+/*
+1. loop through array
+2. create HTML code for each todo 
+3. put HTML on web page
+*/
+
+renderTodoList(); 
+
+function renderTodoList() {
+  let todoListHTML = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todoObject = todoList[i];
+    // destructing 
+    const { name } = todoObject;
+    const { dueDate } = todoObject;
+    const html = `
+    <p>
+    ${name} ${dueDate}
+    <button onclick="
+      todoList.splice(${i},1);
+      renderTodoList();
+    ">Delete</button> 
+    </p>`;
+    todoListHTML += html;
+  }
+  // console.log(todoListHTML);
+
+  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
 }
